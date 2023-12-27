3ea8163ece4dc451f7c4ccb1c05dae62d01a50bd
Barkou abderrahmane <Iman555bpe147@gmail.com>
2023-12-27 22:47:03 +0100

    Started working on the leaderboard + Bug fixes

--- Comment.c
+++ Comment.c
@@ -570,3 +570,17 @@ int main()
    
     return 0;
 }
+
+
+
+void red () {
+  printf("\033[1;31m");
+}
+
+void yellow {
+  printf("\033[1;33m");
+}
+
+void reset () {
+  printf("\033[0m");
+}
--- mainBetter.c
+++ mainBetter.c
@@ -19,6 +19,7 @@
         int defeats;
         int goalsScored;
         int goalsConceded;
+        int rank;
         struct team* next;
     } team; 
 
@@ -31,20 +32,33 @@
     int numTeams;
     int hasCommentator;
 
+    //Color Control
+    void resetCLR();
+    void red();
+    void yellow();
+    void whiteBold();
+    void black();
+    void cyan();
+    void blue();
+    void purple();
+    void green();
+    
 
     //Functions
-    void simulate(team team1, team team2);
+    void simulate(team* team1, team* team2);
     int processAttack(team teamDEF, team teamATK, float bonusDEF, float bonusATK);
     void printArr(team Arr[], int n);
     team dummyFac();
-    void ballin();
+    void* ballin();
     void printHashTable();
     unsigned int hash(char* name);
     int hashTableInsert(team *p);
     int unweightedRNG(int min,int max);
     void initHashTable();
     void initTeamArray(team* teamArray[], int n);
+    void printTable(team Teams[], int n);
     void newGame();
+    void leader();
 
 int main() {
     srand(time(NULL));
@@ -149,6 +163,7 @@ void newGame() {
         Teams[i].goalsScored = 0;
         Teams[i].wins = 0;
         Teams[i].defeats = 0;
+        Teams[i].rank = i;
         hashTableInsert(&Teams[i]);
     }
     memcpy(globalTeams,Teams,sizeof(team)*numTeams);
@@ -158,8 +173,9 @@ void newGame() {
 
 }
 
-void ballin() {
-    
+void* ballin() {
+    // yeah we ballin'
+    char nextCommand;
     
     // Forcing Number of teams to be even
     if (numTeams % 2 != 0) {
@@ -170,16 +186,18 @@ void ballin() {
 
     // Start of the main event loop
     int rounds = numTeams - 1;
-    for (int roundNum = 1; roundNum <= rounds; roundNum++) {
+    int roundNum = 1;
+    while(roundNum <= rounds) {
+        red();
         printf("Round %d:\n", roundNum);
+        resetCLR();
         for (int i = 0; i < numTeams / 2; i++) {
-            team team1 = globalTeams[i];
-            team team2 = globalTeams[numTeams - 1 - i];
-            if (strncmp(team1.name,"Dummy",MAX_NAME) != 0 && strncmp(team2.name,"Dummy",MAX_NAME) != 0 ) {
+            team* team1 = &(globalTeams[i]);
+            team* team2 = &(globalTeams[numTeams - 1 - i]);
+            if (strncmp(team1->name,"Dummy",MAX_NAME) != 0 && strncmp(team2->name,"Dummy",MAX_NAME) != 0 ) {
                 // event happening
-                printf("%s vs %s\n", team1.name, team2.name);
+                printf("%s vs %s\n", team1->name, team2->name);
                 simulate(team1,team2);
-
             }
         }
         team tempTeam = globalTeams[1];
@@ -187,85 +205,159 @@ void ballin() {
             globalTeams[i] = globalTeams[i + 1];
         }
         globalTeams[numTeams - 1] = tempTeam;
-        system("Pause");
+
+        //Showing Help Menu if on 1st Itteration
+        if (roundNum == 1) {
+        printf("Next Command Control Panel:\n");
+        printf("(n) : Continues to the next round\n");
+        printf("(l) : Shows and updates the leaderboards\n");
+        printf("(p) : Prints specific info about a team\n");
+        printf("(s) : Save the current round to a file\n");
+        printf("(q) : Quit The simulation\n");
+        }
+
+        //Fetching next Command
+        int paused = 1;
+        while (paused) {
+            yellow();
+            printf("Enter the next command\n");
+            resetCLR();
+            do {
+                nextCommand = getchar();
+            } while (nextCommand != 'n' && nextCommand != 'l' && nextCommand != 'p' && nextCommand != 's' && nextCommand !=  'q');
+
+            switch (nextCommand) {
+                case 'n':
+                roundNum++;
+                paused = 0;
+                system("Pause");
+                break;
+                case 'l':
+                leader();
+                break;
+                case 'p':
+                // printTeam()
+                break;
+                case 's':
+                // Save()
+                break;
+                case 'q':
+                return NULL;
+                break;
+            }
+        }
+
+        system("cls");
     }
 
 }
 
-void simulate(team team1, team team2) {
-    int w1,w2;
-    w1 = (team1.AttckW + team1.DefW + team1.MidW)/3;
-    w2 = (team2.AttckW + team2.DefW + team2.MidW)/3;
+void leader() {
+    printTable(globalTeams,numTeams);
+}
+
+void printTable(team Teams[], int n) {
+    printf("+---------------------------------------------+\n");
+    printf("|                 LEADERBOARD                  |\n");
+    printf("+---------------------------------------------+\n");
+    printf("| Rank | Team Name        | Points | Wins | Losses |\n");
+    printf("|------+------------------+--------+------+--------|\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("|   %d  | %-16s | %-6d | %-4d | %-6d |\n", i + 1, Teams[i].name, Teams[i].pts, Teams[i].wins, Teams[i].defeats);
+    }
+
+    printf("+---------------------------------------------+\n");
+}
+
+void simulate(team *team1, team *team2) {
+    int w1, w2;
+    w1 = (team1->AttckW + team1->DefW + team1->MidW) / 3;
+    w2 = (team2->AttckW + team2->DefW + team2->MidW) / 3;
 
     // getting the number of encounters
     int encounterNum;
-    encounterNum = abs(((w1 + team1.MidW)/2)-((w2 + team2.MidW)/2));
-    
-    //Goals
+    encounterNum = abs(((w1 + team1->MidW) / 2) - ((w2 + team2->MidW) / 2));
+
+    // Goals
     int Goals1 = 0;
     int Goals2 = 0;
 
-    //Starting the encounter loop
-    int coin = 0; // Side decides who is attacking
-    int result; // stores the result of the encounter
-    int CounterAttack = 0; // counter Attack flag
+    // Starting the encounter loop
+    int coin = 0;            // Side decides who is attacking
+    int result;              // stores the result of the encounter
+    int CounterAttack = 0;   // counter Attack flag
     int Enc = 0;
     while (Enc < encounterNum) {
         if (!CounterAttack) {
-            coin = unweightedRNG(-(w1+team1.MidW),w2+team2.MidW);
+            coin = unweightedRNG(-(w1 + team1->MidW), w2 + team2->MidW);
         } else {
-            coin = (-1)*coin;
+            coin = (-1) * coin;
         }
 
         // Choosing the side
         if (coin > 0) {
             if (!CounterAttack) {
-                result = processAttack(team1,team2,1,1);
+                result = processAttack(*team1, *team2, 1, 1);
             } else {
-                result = processAttack(team1,team2,0.75,1.25);
+                result = processAttack(*team1, *team2, 0.75, 1.25);
                 CounterAttack = 0;
             }
             switch (result) {
                 case 2:
-                Goals2++;
-                team2.goalsScored += 1;
-                team1.goalsConceded += 1;
-                Enc++;
-                break;
+                    Goals2++;
+                    team2->goalsScored += 1;
+                    team1->goalsConceded += 1;
+                    Enc++;
+                    break;
                 case 1:
-                Enc++;
-                break;
+                    Enc++;
+                    break;
                 case 0:
-                CounterAttack = 1;
-                break;
+                    CounterAttack = 1;
+                    break;
             }
 
         } else {
             if (!CounterAttack) {
-                result = processAttack(team2,team1,1,1);
+                result = processAttack(*team2, *team1, 1, 1);
             } else {
-                result = processAttack(team2,team1,0.75,1.25);
+                result = processAttack(*team2, *team1, 0.75, 1.25);
                 CounterAttack = 0;
             }
             switch (result) {
                 case 2:
-                Goals1++;
-                team1.goalsScored += 1;
-                team2.goalsConceded += 1;
-                Enc++;
-                break;
+                    Goals1++;
+                    team1->goalsScored += 1;
+                    team2->goalsConceded += 1;
+                    Enc++;
+                    break;
                 case 1:
-                Enc++;
-                break;
+                    Enc++;
+                    break;
                 case 0:
-                CounterAttack = 1;
-                break;
+                    CounterAttack = 1;
+                    break;
             }
         }
     }
 
-    printf("Final score: %s\t%d-%d\t%s\n",team1.name,Goals1,Goals2,team2.name);
+    if (Goals1 > Goals2) {
+        team1->wins += 1;
+        team1->pts += 3;
+        team2->defeats += 1;
+    } else if (Goals2 > Goals1) {
+        team2->wins += 1;
+        team2->pts += 3;
+        team1->defeats += 1;
+    } else {
+        team1->pts += 1;
+        team2->pts += 1;
+    }
+
+    printf("Final score: %s\t%d-%d\t%s\n", team1->name, Goals1, Goals2, team2->name);
 }
+
 
 void initTeamArray(team* teamArray[], int n) {
     for (int i=0; i<n; i++) {
@@ -355,4 +447,40 @@ int processAttack(team teamDEF, team teamATK, float bonusDEF, float bonusATK) {
     } else {
         return 0;
     }
+}
+
+void red() {
+  printf("\033[1;31m");
+}
+
+void black() {
+  printf("\033[1;30m");
+}
+
+void green() {
+  printf("\033[1;32m");
+}
+
+void blue() {
+  printf("\033[1;34m");
+}
+
+void purple() {
+  printf("\033[1;35m");
+}
+
+void cyan() {
+  printf("\033[1;36m");
+}
+
+void whiteBold() {
+  printf("\033[1;37m");
+}
+
+void yellow() {
+  printf("\033[1;33m");
+}
+
+void resetCLR() {
+  printf("\033[0m");
 }
