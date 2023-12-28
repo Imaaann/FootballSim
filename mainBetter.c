59cc4d4b958ce530f0e60ea484a3bb084a19aa00
Barkou abderrahmane <Iman555bpe147@gmail.com>
2023-12-28 20:57:07 +0100

    Added leaderboard and Team showcase

--- mainBetter.c
+++ mainBetter.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <windows.h>
 #include <ctype.h>
+#include <string.h>
 
 
     #define MAX_NAME 64
@@ -43,18 +44,32 @@
     void purple();
     void green();
     
+    //HashTable Functions
+    void printHashTable();
+    void initHashTable();
+    unsigned int hash(char* name);
+    int hashTableInsert(team *p);
+    team* hashTableLookup(char* name);
+    team* hashTableDelete(char* name);
+
 
     //Functions
+    void printStrArr(char* str);
+    team* linearSearchByName(char* targetName);
+    void requestTeam();
+    void printTeam(team* team);
+    void strnLower(char* str);
+    team* hashTableLookup(char* name);
+    void bubbleSort(team arr[], int n); // for GD 
+    void quickSort(team arr[], int low, int high); // for pts
+    int partition(team arr[], int low, int high);
+    void swap(team* a, team* b);
     void simulate(team* team1, team* team2);
     int processAttack(team teamDEF, team teamATK, float bonusDEF, float bonusATK);
     void printArr(team Arr[], int n);
     team dummyFac();
     void* ballin();
-    void printHashTable();
-    unsigned int hash(char* name);
-    int hashTableInsert(team *p);
     int unweightedRNG(int min,int max);
-    void initHashTable();
     void initTeamArray(team* teamArray[], int n);
     void printTable(team Teams[], int n);
     void newGame();
@@ -69,12 +84,11 @@ int main() {
     printf("How many teams do you want in the game : ");
     do {
     scanf("%d",&numTeams);
-    if (numTeams > 63) {
+    if (numTeams > MAX_TABLE_SIZE) {
         printf("Too many teams, consider lowering it\n");
     }
-    } while (numTeams > 63);
+    } while (numTeams > MAX_TABLE_SIZE);
     newGame();
-    system("Pause");
     system("cls");
     ballin();
 
@@ -98,6 +112,7 @@ void newGame() {
         printf("Enter the name of the team number %d\n",i+1);
         scanf(" %[^\n]s",buffer);
         trash = getchar();
+        strnLower(buffer);
         strncpy(Teams[i].name,buffer,MAX_NAME);
     }
 
@@ -164,18 +179,22 @@ void newGame() {
         Teams[i].wins = 0;
         Teams[i].defeats = 0;
         Teams[i].rank = i;
-        hashTableInsert(&Teams[i]);
     }
     memcpy(globalTeams,Teams,sizeof(team)*numTeams);
 
     system("cls");
-    printHashTable();
-
 }
 
 void* ballin() {
     // yeah we ballin'
     char nextCommand;
+
+    for (int i=0; i<numTeams;i++) {
+        printf("Team %d: %s --- %p\n", i + 1, globalTeams[i].name, &globalTeams[i]);
+        hashTableInsert(&globalTeams[i]);
+    }
+
+    printHashTable();
     
     // Forcing Number of teams to be even
     if (numTeams % 2 != 0) {
@@ -236,7 +255,7 @@ void* ballin() {
                 leader();
                 break;
                 case 'p':
-                // printTeam()
+                requestTeam();
                 break;
                 case 's':
                 // Save()
@@ -253,21 +272,95 @@ void* ballin() {
 }
 
 void leader() {
-    printTable(globalTeams,numTeams);
+    team sortedTeams[numTeams+1];
+    //pruning the array from globalTeams
+    int rnk;
+    for (int i=0; i<numTeams; i++) {
+        rnk = globalTeams[i].rank;
+        sortedTeams[rnk] = globalTeams[i];
+    }
+
+    //Sorting the array according to pts (using quick sort)
+    quickSort(sortedTeams,0,numTeams-1);
+    bubbleSort(sortedTeams,numTeams);
+
+    // printing the leaderboard
+    printTable(sortedTeams,numTeams);
+
+    //assigning the new ranks
+    team* tmp = NULL;
+
+    for (int i = 0; i < numTeams; i++) {
+        tmp = linearSearchByName(sortedTeams[i].name);
+        if (tmp != NULL) {
+            tmp->rank = i;
+        }
+    }
+}
+
+void requestTeam() {
+
+    char buffer[MAX_NAME];
+    char trash;
+
+    purple();
+    printf("Enter the name of the team you want to check\n");
+    resetCLR();
+    scanf(" %[^\n]s",buffer);
+    trash = getchar();
+    strnLower(buffer);
+
+    team* tmp = NULL;
+    tmp = hashTableLookup(buffer);
+    if (tmp != NULL) {
+        printTeam(tmp);
+    } else {
+        red();
+        printf("The Team Doesn't exist\n");
+        resetCLR();
+    }
+
+}
+
+void printTeam(team* teamPtr) {
+    printf(" +------------------------------------------------------+\n");
+    printf(" | %-23s League: %s |\n", teamPtr->name, leagueName);
+    printf(" |  Points: %-4d | Rank: %-3d | Wins: %-3d | Defeats: %-3d | Goals: %d-%d |\n", teamPtr->pts, teamPtr->rank, teamPtr->wins, teamPtr->defeats, teamPtr->goalsScored, teamPtr->goalsConceded);
+    printf(" | AttckW: %-3d |  MidW: %-3d | DefW: %-3d | GD: %3d   |\n", teamPtr->AttckW, teamPtr->MidW, teamPtr->DefW, teamPtr->goalsScored - teamPtr->goalsConceded);
+    printf(" +------------------------------------------------------+\n");
 }
 
 void printTable(team Teams[], int n) {
-    printf("+---------------------------------------------+\n");
-    printf("|                 LEADERBOARD                  |\n");
-    printf("+---------------------------------------------+\n");
-    printf("| Rank | Team Name        | Points | Wins | Losses |\n");
-    printf("|------+------------------+--------+------+--------|\n");
+    printf("+-------------------------------------------------------------+\n");
+    printf("|                          LEADERBOARD                        |\n");
+    printf("+-------------------------------------------------------------+\n");
+    printf("| Rank | Team Name           | Points | Wins | Losses | GD   |\n");
+    printf("|------+---------------------+--------+------+--------+------|\n");
 
+    
     for (int i = 0; i < n; i++) {
-        printf("|   %d  | %-16s | %-6d | %-4d | %-6d |\n", i + 1, Teams[i].name, Teams[i].pts, Teams[i].wins, Teams[i].defeats);
+        if (strncmp(Teams[i].name,"Dummy",MAX_NAME) != 0) {
+            int goalDifference = Teams[i].goalsScored - Teams[i].goalsConceded;
+            int rankWidth = (i + 1 < 10) ? 2 : 1;
+
+            if (Teams[i].rank > i) {
+                green();
+            } else if (Teams[i].rank < i) {
+                red();
+            }
+
+
+            if (i == 0) { // throughout heaven and earth I alone am the honored one
+                yellow();
+            }
+
+            printf("|   %-*d | %-19s | %-6d | %-4d | %-6d | %+4d |\n", rankWidth, i + 1, Teams[i].name, Teams[i].pts, Teams[i].wins, Teams[i].defeats, goalDifference);
+            resetCLR();
+        }
     }
 
-    printf("+---------------------------------------------+\n");
+
+    printf("+-------------------------------------------------------------+\n");
 }
 
 void simulate(team *team1, team *team2) {
@@ -358,60 +451,14 @@ void simulate(team *team1, team *team2) {
     printf("Final score: %s\t%d-%d\t%s\n", team1->name, Goals1, Goals2, team2->name);
 }
 
-
 void initTeamArray(team* teamArray[], int n) {
     for (int i=0; i<n; i++) {
         teamArray[i] = NULL;
     }
 }
 
-void initHashTable() {
-    for (int i = 0; i<MAX_TABLE_SIZE; i++) {
-        hash_Table[i] = NULL;
-    }
-}
-
 int unweightedRNG(int min,int max) {
     return ((rand()%(max-min+1))+min);
-}
-
-int hashTableInsert(team *p) {
-    if (p == NULL) {
-        return 0;
-    }
-    int index = hash(p -> name);
-    p -> next = hash_Table[index];
-    hash_Table[index] = p;
-    return 1;
-} 
-
-unsigned int hash(char* name) {
-    int strLength = strnlen(name, MAX_NAME);
-    unsigned int hashValue = 0;
-    for (int i=0; i<strLength; i++) {
-        hashValue += name[i];
-        hashValue = ( hashValue * name[i]) % MAX_TABLE_SIZE;
-
-    }
-    return hashValue;
-}
-
-void printHashTable() {
-    printf("New Hash Table Print\n");
-    for (int i = 0; i<MAX_TABLE_SIZE; i++) {
-        if (hash_Table[i] == NULL) {
-            printf("\t%i\t----\n",i);
-        } else {
-            printf("\t%i\t-",i);
-            team* tmp = hash_Table[i];
-            while (tmp != NULL) {
-                printf("%s (Def: %d, Mid: %d, Attck: %d)-", tmp -> name,tmp->DefW,tmp->MidW,tmp->AttckW);
-                tmp = tmp -> next;
-            }
-            printf("\n");
-        }
-    }
-    printf("End of hash Table Print\n");
 }
 
 team dummyFac() {
@@ -423,6 +470,7 @@ team dummyFac() {
     Dummy.goalsScored = 0;
     Dummy.wins = 0;
     Dummy.defeats = 0;
+    Dummy.rank = numTeams;
 
     return (Dummy);
 }
@@ -449,38 +497,184 @@ int processAttack(team teamDEF, team teamATK, float bonusDEF, float bonusATK) {
     }
 }
 
+void swap(team* a, team* b) {
+    team temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(team arr[], int low, int high) {
+    int pivot = arr[high].pts;
+    int indx = low - 1; 
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j].pts >= pivot) {
+            indx++;
+            swap(&arr[indx], &arr[j]);
+        }
+    }
+
+    swap(&arr[indx + 1], &arr[high]);
+    return indx + 1;
+}
+
+void quickSort(team arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void bubbleSort(team arr[], int n) { // bubble sort for GD
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            int isNotDummy = (strncmp(arr[j].name,"Dummy",MAX_NAME) != 0 && strncmp(arr[j+1].name,"Dummy",MAX_NAME) != 0) ? 1:0;
+            int GD1 = arr[j].goalsScored - arr[j].goalsConceded;
+            int GD2 = arr[j+1].goalsScored - arr[j+1].goalsConceded;
+            if (arr[j].pts == arr[j+1].pts && (GD1 <= GD2) && isNotDummy) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void strnLower(char* str) {
+    int length = strlen(str);
+    str[0]=toupper(str[0]);
+    for (int i=1; i<length; i++) {
+        str[i] = tolower(str[i]);
+    }
+}
+
+team* linearSearchByName(char* targetName) {
+    for (int i = 0; i < numTeams; i++) {
+        if (strcmp(globalTeams[i].name, targetName) == 0) {
+            return &globalTeams[i]; // Return a pointer to the found team
+        }
+    }
+    return NULL; // Return NULL if the team with the specified name is not found
+}
+
+void printStrArr(char* str) {
+    int len = strlen(str);
+    printf("[");
+    for (int i=0; i<(len-1); i++) {
+        printf("%c ,", str[i] );
+    }
+    printf("%c]\n", str[len-1]);
+}
+
+//HashTable Functions
+void printHashTable() {
+    printf("New Hash Table Print\n");
+    for (int i = 0; i<MAX_TABLE_SIZE; i++) {
+        if (hash_Table[i] == NULL) {
+            printf("\t%i\t----\n",i);
+        } else {
+            printf("\t%i\t-",i);
+            team* tmp = hash_Table[i];
+            while (tmp != NULL) {
+                printf("%s (pointer: %p) -", tmp -> name, tmp);
+                tmp = tmp -> next;
+            }
+            printf("\n");
+        }
+    }
+    printf("End of hash Table Print\n");
+}
+
+unsigned int hash(char* name) {
+    int strLength = strnlen(name, MAX_NAME);
+    unsigned int hashValue = 0;
+    for (int i=0; i<strLength; i++) {
+        hashValue += name[i];
+        hashValue = ( hashValue * name[i]) % MAX_TABLE_SIZE;
+
+    }
+    return hashValue;
+}
+
+int hashTableInsert(team *p) {
+    if (p == NULL) {
+        return 0;
+    }
+    int index = hash(p -> name);
+    p -> next = hash_Table[index];
+    hash_Table[index] = p;
+    return 1;
+} 
+
+team* hashTableLookup(char* name) {
+    int index = hash(name);
+    team *tmp = hash_Table[index];
+
+    while (tmp != NULL) {
+        team tmpTeam;
+        memcpy(&tmpTeam, tmp, sizeof(team));
+
+        if (strncmp(tmpTeam.name, name, MAX_NAME) == 0) {
+            return tmp;
+        }
+
+        tmp = tmp->next;
+    }
+
+    return NULL;
+}
+
+team* hashTableDelete(char* name) {
+    int index = hash(name);
+    team* tmp = hash_Table[index];
+    team* prv = NULL;
+    while (tmp != NULL && strncmp(tmp -> name, name, MAX_NAME) != 0) 
+    {
+        prv = tmp;
+        tmp = tmp -> next;
+    }
+    if (tmp == NULL) return NULL;
+    if (prv == NULL) {
+        // delete head of linked list by pushing the next on in the linked list to the first
+        hash_Table[index] = tmp -> next;
+    } else {
+        prv -> next = tmp -> next; // delete node at tmp by pushing the pointers away from it
+    } 
+    return tmp;
+}
+
+void initHashTable() {
+    for (int i = 0; i<MAX_TABLE_SIZE; i++) {
+        hash_Table[i] = NULL;
+    }
+    // table is Empty Yiiipee
+}
+
+// colors 
 void red() {
   printf("\033[1;31m");
 }
-
 void black() {
   printf("\033[1;30m");
 }
-
 void green() {
   printf("\033[1;32m");
 }
-
 void blue() {
   printf("\033[1;34m");
 }
-
 void purple() {
   printf("\033[1;35m");
 }
-
 void cyan() {
   printf("\033[1;36m");
 }
-
 void whiteBold() {
   printf("\033[1;37m");
 }
-
 void yellow() {
   printf("\033[1;33m");
 }
-
 void resetCLR() {
   printf("\033[0m");
 }
